969586c3a36dfb81e4a304b25610295fff383fd0
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-05 21:32:06 +0800

    feat: create getDaysInMonth function

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,7 @@
 let userInput = document.querySelector("#date");
 userInput.max = new Date().toISOString().split("T")[0];
 
+function getDaysInMonth (year, month) {
+  return new Date(year, month, 0).getDate();
+}
+
